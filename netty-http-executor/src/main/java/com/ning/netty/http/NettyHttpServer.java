963820286b5b1dfee0f4cc727abf34c87d7eed68
guotongning <guotongning@58.com>
2021-10-20 11:00:53 +0800

    add some Notes

--- netty-http-executor/src/main/java/com/ning/netty/http/NettyHttpServer.java
+++ netty-http-executor/src/main/java/com/ning/netty/http/NettyHttpServer.java
@@ -23,20 +23,23 @@ public class NettyHttpServer {
 
     public static void main(String[] args) {
         // configure the server
+        // 1. 初始化用于Acceptor的 main线程池 以及用于I/O工作的 slave线程池
         EventLoopGroup group = new NioEventLoopGroup(1);
         EventLoopGroup worker = new NioEventLoopGroup();
         try {
+            //https://www.cnblogs.com/itdriver/p/8149913.html 《Netty4.x 源码实战系列（一）： 深入理解ServerBootstrap 与 Bootstrap》
+            // 2. 初始化ServerBootstrap实例，此实例是netty服务端应用开发的入口。
             ServerBootstrap b = new ServerBootstrap();
-            b.option(ChannelOption.SO_BACKLOG, 1024);
-            b.childOption(ChannelOption.TCP_NODELAY, true);
-            b.childOption(ChannelOption.SO_KEEPALIVE, true);
-            b.group(group, worker)
-                    .channel(NioServerSocketChannel.class)
-                    .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new NettyHttpServerInitializer());
-            Channel channel = b.bind(PORT).sync().channel();
+            b.group(group, worker)// 3. 通过ServerBootstrap的group方法设置初始化的线程池
+                    .channel(NioServerSocketChannel.class)// 4. 指定通道channel的类型，由于是服务端，故而是NioServerSocketChannel
+                    .handler(new LoggingHandler(LogLevel.INFO))// 5. 设置ServerSocketChannel的处理器
+                    .childHandler(new NettyHttpServerInitializer())// 6. 设置子通道也就是SocketChannel的处理器， 其内部是实际业务开发的"主战场"
+                    .option(ChannelOption.SO_BACKLOG, 1024)// 7. 配置ServerSocketChannel的选项
+                    .childOption(ChannelOption.TCP_NODELAY, true)// 8. 配置子通道也就是SocketChannel的选项
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .bind(PORT).sync().channel()// 9. 绑定端口
+                    .closeFuture().sync(); // 10. 监听端口
             logger.info("Netty http server listening on port " + PORT);
-            channel.closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
